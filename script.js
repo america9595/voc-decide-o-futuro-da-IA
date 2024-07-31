ce3773fcec922c51d9377c5a4e194d935d6486bc
america9595 <127946009+america9595@users.noreply.github.com>
2024-07-31 08:03:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const caixaprincipal = document = querySelector(".caixa-principal");
+const caixaperguntas = document = querySelector(".caixa-perguntas");
+const caixaalternativas = document = querySelector(".caixa-alternativas");
+const caixaresultados = document = querySelector(".caixa-resultados");
+const textoresultado = document = querySelector(".texto-resultado");
+
+
+
+const perguntas = [
+    {
+        enunciado: "pergunta 1",
+        alternativa:[
+            "alternativa 1"
+            "alternativa 2"
+            "alternativa 3"
+            "alternativa 4"
+            "alternativa 5"
+        ]
+    }
+]
